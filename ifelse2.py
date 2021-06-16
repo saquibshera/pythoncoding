df2add791e543711bbc4d2dcf06ada808fff3732
saquibshera <71259201+saquibshera@users.noreply.github.com>
2021-06-16 22:30:26 +0530

    Add files via upload

---
+++ ifelse2.py
@@ -0,0 +1,8 @@
+number=int(input("Enter the number"))
+
+if number>0 and number<=10:
+    print("good result")
+elif number>10 and number<=20:
+    print("awesome result")
+else:
+    print("not in range")
