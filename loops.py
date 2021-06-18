99df0021a8aa40be6d25b0523328136cf77cdbac
saquibshera <71259201+saquibshera@users.noreply.github.com>
2021-06-18 23:40:35 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,14 @@
+
+names=['nasib','saquib','mifzal']
+
+# for i in range(names.__len__()):
+#     print(names[i])
+#
+# for name in names:
+#     print(name)
+
+i=0
+while i<names.__len__():
+    print(names[i])
+    i+=1
+
