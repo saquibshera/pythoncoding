80f6733874267733fa2ef0ecca881c5160463453
saquibshera <71259201+saquibshera@users.noreply.github.com>
2021-07-08 22:55:41 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,18 @@
+def add():
+    print("addition one")
+    num1=int(input("Enter number 1"))
+    num2=int(input("Enter number 2"))
+    res=num1+num2
+    print(res)
+
+def sub():
+    print("subtraction")
+    num1 = int(input("Enter number 1"))
+    num2 = int(input("Enter number 2"))
+    res = num1 - num2
+    print(res)
+add()
+sub()
+
+
+
